f0da7e591df7c4a515b4a1ec7b3a468cba493067
sauravpandey538 <sauravpandey0325@icloud.com>
2024-05-28 10:17:50 +0545

    trying to fix error

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-    res.send('Hello, Express 2.0!');
+    res.send('Hello, Express 3.0!');
+});
+app.get('/hello', (req, res) => {
+    res.send('Hello,  3.0!');
 });
 
 // Start the server and listen on port 3000
