c1e99bcd9a3a7e6a5790313dd03105c5655cf25a
sauravpandey538 <sauravpandey0325@icloud.com>
2024-06-30 17:41:51 +0545

    same site none

--- api/index.js
+++ api/index.js
@@ -86,6 +86,7 @@ app.post("/login", async (req, res) => {
         httpOnly: true,
         secure: true,
         maxAge: 24 * 60 * 60 * 1000,
+        sameSite:'None'
         // path: '/api' // 24 hours
       };
   
