d262ab6ec7e040c7420ce238c5d74ca0e9042d14
sauravpandey538 <sauravpandey0325@icloud.com>
2024-06-30 20:47:02 +0545

    sd

--- api/index.js
+++ api/index.js
@@ -86,7 +86,7 @@ app.post("/login", async (req, res) => {
         httpOnly: true,
         secure: false, // changing to true gives problem to store cookie in mobile
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite:'None'
+        sameSite:'Lax'
         // path: '/api' // 24 hours
       };
   
